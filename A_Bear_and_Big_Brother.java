d98e7e227ad6094820dc5d3badfb4810237487d4
Firaol Seboka <157240300+Firaol-9@users.noreply.github.com>
2026-02-12 11:52:19 +0300

    A_Bear_and_Big_Brother

---
+++ A_Bear_and_Big_Brother.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class A_Bear_and_Big_Brother{
+   public static void  main(String [] args){
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int counter = 0;
+
+        while ( a <= b){
+            a *= 3;
+            b *= 2;
+            counter++;
+        }
+        System.out.println(counter);
+   }
+}
